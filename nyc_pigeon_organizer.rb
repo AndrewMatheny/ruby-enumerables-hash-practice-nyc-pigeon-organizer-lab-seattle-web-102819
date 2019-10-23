a392637c75c9aa34975e6372ea6caae3d8325db5
Andrew Matheny <androidmatheny@gmail.com>
2019-10-23 03:45:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
-  pigeon_list
+  pigeon_list = {}
+  data.each do |property, hash|
+    hash.each do |attribute, array|
+      array.each do |name|
+        if !pigeon_list.has_key(name)
+          pigeon_list[name] = {}
+        end
+        if !pigeon_list[name][property]
+          pigeon_list[name][property] = []
+        end
+        pigeon_list[name][property].push(attribute.to_s)
+      end
+    end
 end
